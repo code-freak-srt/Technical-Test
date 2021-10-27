c71bfa9391c217d82acab82af95fb869a019dc00
code-freak-srt <sushilrajthala421@gmail.com>
2021-10-27 20:32:59 +0545

    Correction Made

--- tes1.js
+++ tes1.js
@@ -5,11 +5,11 @@ function result(input) {
     let totalScore = 0;
     for (i = 0; i < input.length; i++) {
         if (input[i] == 0 || input[i] % 2 == 0) {
-            input[i] = input[i] + 1;
+            input[i] = 1;
         } else if (input[i] % 2 == 1 && input[i] != 5) {
-            input[i] = input[i] + 3;
+            input[i] = 3;
         } else if (input[i] == 5) {
-            input[i] == input[i] + 5;
+            input[i] == 5;
         }
         totalScore = totalScore + input[i];
     }
